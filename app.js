f4d298f25cae3ab3b31ec4d14d45a98ee7258416
DannyP95 <palaudanny@gmail.com>
2023-07-25 20:04:32 -0300

    Primera subida

---
+++ app.js
@@ -0,0 +1,36 @@
+// const express = require('express');
+// const fs = require('fs');
+// const path = require('path');
+
+
+// const app = express();
+// const puerto = 3001;
+
+// app.use(express.json());
+// app.use('/', express.static(__dirname + '/public'));
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// app.listen(puerto, () => {
+//     console.log('Aplicación esuchando en puerto 3001');
+// });
+
+// ==========> requerimos los modulos para trabajar
+const path = require("path");
+const express = require("express");
+const app = express();
+// ==========> creamos la ruta a los archivos estaticos 
+app.use(express.json());
+app.use ('/', express.static(__dirname + '/public'))
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+// ==========> creamos el servidor y le pasamos un mensaje para verificar su funcionalidad
+
+app.listen(3333,()=>{
+    console.log("El servidor esta funcionando")
+})
